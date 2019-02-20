195c2c21a36c3e61e0a63f77e433aec89814632d
David Tai <david@verus.io>
2019-02-20 17:29:47 -0600

    build single index file using webpack

---
+++ webpack/webpack-prod.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+const root = path.resolve(__dirname, '..');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: root + '/lib',
+    filename: 'index.js'
+  }
+};
